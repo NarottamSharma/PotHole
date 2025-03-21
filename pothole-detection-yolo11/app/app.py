5adb03545ee2a7eaa0174719dae6b439a53afb5b
Narottam Sharma <narottamaiengineer@gmail.com>
2025-03-21 15:54:52 +0000

    code updated

--- pothole-detection-yolo11/app/app.py
+++ pothole-detection-yolo11/app/app.py
@@ -86,7 +86,7 @@ def detect_video():
                 x1, y1, x2, y2 = box.xyxy[0]
                 conf = box.conf[0]
                 label = r.names[int(box.cls[0])]
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2, int(y2))), (0, 255, 0), 2)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
                 cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1) - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         
